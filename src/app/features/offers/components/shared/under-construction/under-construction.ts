263e46de6fb594d5c3454f9b02fdd4f348be9ec2
satheeshpolu <satheeshpoluab@gmail.com>
2026-01-21 00:12:41 +0100

    minor fix

--- src/app/features/offers/components/shared/under-construction/under-construction.ts
+++ src/app/features/offers/components/shared/under-construction/under-construction.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { BackButton } from '../back-button/back-button';
 
 @Component({
   selector: 'under-construction',
-  imports: [MatIconModule],
+  imports: [MatIconModule, BackButton],
   templateUrl: './under-construction.html',
   styleUrl: './under-construction.css',
 })
